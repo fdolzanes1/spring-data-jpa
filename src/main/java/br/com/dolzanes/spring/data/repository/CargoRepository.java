5320270e0129f7fe70bd47a196f416c5af169a9c
fdolzanes1 <fdolzanes1@gmail.com>
2022-12-22 15:28:10 -0400

    Inserting values

---
+++ src/main/java/br/com/dolzanes/spring/data/repository/CargoRepository.java
@@ -0,0 +1,11 @@
+package br.com.dolzanes.spring.data.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.dolzanes.spring.data.orm.Cargo;
+
+@Repository
+public interface CargoRepository extends CrudRepository<Cargo, Integer> {
+
+}
